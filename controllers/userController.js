ae7566c6f1b2bf63e1ad2b5205bbe1b83285b88b
atmiktumane <atmiktumane72@gmail.com>
2024-06-03 18:11:20 +0530

    added logic for User Registration and Password Hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,49 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc Register the User
 //route POST "/api/users/register"
 //access Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register the user" });
+    const { username, email, password } = req.body;
+
+    // Simple Validation
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All the fields are mandatory");
+    }
+
+    // Check if user already exists or not
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        res.status(400);
+        throw new Error("User already exists!");
+    }
+
+    // Hashed Password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // console.log("Hashed Password : ", hashedPassword);
+
+    // Create/register new user and save it in MongoDb database
+    const newUser = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    console.log(`New User created & stored in Database : ${newUser}`);
+
+
+    if (newUser) {
+        res.status(201).json({ _id: newUser.id, email: newUser.email });
+    } else {
+        res.status(400);
+        throw new Error("User data is not valid");
+    }
+
+
+    // res.json({ message: "Register the user" });
 });
 
 //@desc login User
